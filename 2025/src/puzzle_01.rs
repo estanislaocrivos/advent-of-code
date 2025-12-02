a761222e5a44bc999d12c1625e6ac1418543b0b7
estanislaocrivos <estanislaocrivos@gmail.com>
2025-12-01 21:45:52 -0300

    Updates on puzzle 1.

--- 2025/src/puzzle_01.rs
+++ 2025/src/puzzle_01.rs
@@ -4,59 +4,52 @@ struct Knob {
 }
 
 impl Knob {
-    fn move_right(&mut self, amount: u32) -> u32 {
+    fn new(initial_state: u32) -> Knob {
+        return Knob {
+            state: initial_state,
+            zero_passes: 0,
+        };
+    }
+
+    fn move_right(&mut self, amount: u32) {
+        if amount > 99 {
+            self.zero_passes += amount / 100;
+        }
         let effective_amount = amount % 100;
         if effective_amount > self.state {
-            return 100 - (effective_amount - self.state);
+            self.zero_passes += 1;
+            self.state = 100 - (effective_amount - self.state);
         } else {
-            return self.state - effective_amount;
+            self.state = self.state - effective_amount;
         }
     }
 
-    fn move_left(&mut self, amount: u32) -> u32 {
+    fn move_left(&mut self, amount: u32) {
+        if amount > 99 {
+            self.zero_passes += amount / 100;
+        }
         let effective_amount = amount % 100;
         let result = self.state + effective_amount;
         if result > 99 {
-            return result - 100;
+            self.zero_passes += 1;
+            self.state = result - 100;
         } else {
-            return result;
+            self.state = result;
         }
     }
 
-    fn get_state(self) -> u32 {
+    fn get_state(&self) -> u32 {
         return self.state;
     }
 
-    fn get_zero_passes(self) -> u32 {
+    fn get_zero_passes(&self) -> u32 {
         return self.zero_passes;
-    }
-}
-
-fn move_knob_left(knob_state: u32, amount: u32) -> u32 {
-    let effective_amount = amount % 100;
-    if effective_amount > knob_state {
-        return 100 - (effective_amount - knob_state);
-    } else {
-        return knob_state - effective_amount;
-    }
-}
-
-fn move_knob_right(knob_state: u32, amount: u32) -> u32 {
-    let effective_amount = amount % 100;
-    let result = knob_state + effective_amount;
-    if result > 99 {
-        return result - 100;
-    } else {
-        return result;
     }
 }
 
 pub fn puzzle_01_01() {
     let instructions_to_decode = include_str!("puzzle_01_input.txt");
-
-    // The knob goes from 0 to 99, it has 100 positions. A move of 5 left from 0 results in -5, and a move of 10 from -5 results in 5
-
-    let mut knob_state: u32 = 50;
+    let mut knob = Knob::new(50);
     let mut password: u32 = 0;
 
     for instruction in instructions_to_decode.lines() {
@@ -64,16 +57,37 @@ pub fn puzzle_01_01() {
         let amount: u32 = instruction[1..].parse().expect("Not a valid number!");
         println!("Instruction: {knob_direction} by {amount}");
         if knob_direction == "R" {
-            knob_state = move_knob_right(knob_state, amount);
+            knob.move_right(amount);
         }
         if knob_direction == "L" {
-            knob_state = move_knob_left(knob_state, amount);
+            knob.move_left(amount);
         }
-        if knob_state == 0 {
+        if knob.get_state() == 0 {
             password += 1;
         }
     }
     println!("The password for Puzzle 01 Part 01 is: {password}");
 }
 
-pub fn puzzle_01_02() {}
+pub fn puzzle_01_02() {
+    let instructions_to_decode = include_str!("puzzle_01_input.txt");
+    let mut knob = Knob::new(50);
+    let mut password: u32 = 0;
+
+    for instruction in instructions_to_decode.lines() {
+        let knob_direction = &instruction[0..1];
+        let amount: u32 = instruction[1..].parse().expect("Not a valid number!");
+        println!("Instruction: {knob_direction} by {amount}");
+        if knob_direction == "R" {
+            knob.move_right(amount);
+        }
+        if knob_direction == "L" {
+            knob.move_left(amount);
+        }
+        if knob.get_state() == 0 {
+            password += 1;
+        }
+    }
+    password += knob.get_zero_passes();
+    println!("The password for Puzzle 01 Part 02 is: {password}");
+}
