3be1bb8e0f7b2f3d36ce8c7d1a3b21fc4d603a8e
estanislaocrivos <estanislaocrivos@gmail.com>
2025-12-10 23:04:53 -0300

    Updates on puzzle 3.

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -3,12 +3,13 @@ mod puzzle_02;
 mod puzzle_03;
 use puzzle_01::{puzzle_01_01, puzzle_01_02};
 use puzzle_02::{puzzle_02_01, puzzle_02_02};
-use puzzle_03::puzzle_03_01;
+use puzzle_03::{puzzle_03_01, puzzle_03_02};
 
 fn main() {
-    puzzle_01_01();
-    puzzle_01_02();
-    puzzle_02_01();
-    puzzle_02_02();
+    // puzzle_01_01();
+    // puzzle_01_02();
+    // puzzle_02_01();
+    // puzzle_02_02();
     puzzle_03_01();
+    puzzle_03_02();
 }
--- 2025/src/puzzle_03.rs
+++ 2025/src/puzzle_03.rs
@@ -1,35 +1,43 @@
 use std::usize;
 
-const DEBUG_LOG_EN: bool = false;
+const DEBUG_LOG_EN: bool = true;
+const TEST_EXAMPLE: bool = true;
 
 macro_rules! puzzle_input {
     () => {
-        include_str!("../resources/puzzle_03_input.txt")
+        if TEST_EXAMPLE {
+            include_str!("../resources/puzzle_03_input_example.txt")
+        } else {
+            include_str!("../resources/puzzle_03_input.txt")
+        }
     };
 }
 
+// For this part a simple double-iteration works. Another approach would be to take the biggest numbers in the bank and arrange them big * 10 + small (approach followed on part 2)
 pub fn puzzle_03_01() {
     let battery_banks = puzzle_input!();
     let mut total_joltage: usize = 0;
-    let mut higher_joltage: usize = 0;
+    let mut max_joltage: usize = 0;
+
     for bank in battery_banks.lines() {
         for (i, batt_digit_1) in bank.chars().enumerate() {
             for batt_digit_2 in bank.chars().skip(i + 1) {
                 if let (Some(d1), Some(d2)) = (batt_digit_1.to_digit(10), batt_digit_2.to_digit(10))
                 {
                     let joltage: usize = d1 as usize * 10 + d2 as usize;
-                    if joltage > higher_joltage {
-                        higher_joltage = joltage;
+                    if joltage > max_joltage {
+                        max_joltage = joltage;
                     }
                 }
             }
         }
         if DEBUG_LOG_EN {
-            println!("The higher joltage for bank {} is {}", bank, higher_joltage);
+            println!("The highest joltage for bank {} is {}", bank, max_joltage);
         }
-        total_joltage += higher_joltage;
-        higher_joltage = 0;
+        total_joltage += max_joltage;
+        max_joltage = 0;
     }
+
     println!(
         "The total joltage for Puzzle 03 Part 01 is: {}",
         total_joltage
